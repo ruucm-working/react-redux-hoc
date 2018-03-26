3de0c4bd77c5aa1f89c570b82865fc2ed2f8341e
ruucm <ruucm.a@gmail.com>
2018-03-26 17:19:03 +0900

    add recompose

--- src/routes/TestInput/components/TestInput.js
+++ src/routes/TestInput/components/TestInput.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Select from 'react-select';
+import { connect } from 'react-redux'
+import { increment, doubleAsync } from '../modules/counter'
+import { compose } from 'redux'
+import { lifecycle } from 'recompose'
+import { log } from 'ruucm-util'
 
 export const TestInput = ({ counter, increment, doubleAsync }) => (
   <div style={{ margin: '0 auto' }} >
@@ -20,4 +25,22 @@ TestInput.propTypes = {
   doubleAsync: PropTypes.func.isRequired,
 }
 
-export default TestInput
+// export default TestInput
+const mapDispatchToProps = {
+  increment : () => increment(1),
+  doubleAsync
+}
+
+const mapStateToProps = (state) => ({
+  counter : state.counter
+})
+
+// Component enhancer
+const enhance = compose(
+  lifecycle({
+    componentDidMount() {
+      log('hey');
+    },
+  }),
+)
+export default enhance(TestInput)
--- src/routes/TestInput/index.js
+++ src/routes/TestInput/index.js
@@ -22,3 +22,5 @@ export default (store) => ({
     }, 'counter')
   }
 })
+
+
